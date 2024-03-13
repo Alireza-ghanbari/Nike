15283f48bf61a3c5fc97d352a2efa6f75b369de8
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-03-13 13:33:49 +0330

    create sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
+import{
+  Hero,
+  PopularProducts ,
+  SuperQuality ,
+  Services ,
+  SpecialOffer ,
+  CustomerReviews ,
+  Subscribe ,
+  Footer 
+} from './sections/index'
+
 const App=()=>(
   <main className="relative">
-    Nav
+    {/* <Nav /> */}
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero />
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts />
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality />
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services />
     </section>
     <section className="padding">
-      SpeciaOffer
+      <SpecialOffer />
     </section>
     <section className="bg-pale-blue padding">
-      CustomerReviews
+      <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe />
     </section>
     <section className="bg-black padding-x padding-t pt-8">
-      Footer
+      <Footer />
     </section>
   </main>
 )
